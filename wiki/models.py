b385de5c937e4cfc1707789924c7f63a6fcce019
Tyler Davis <tylerrusselldavis@gmail.com>
2021-09-27 15:41:30 -0500

    Started project, installed Django. Hello World!

---
+++ wiki/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
