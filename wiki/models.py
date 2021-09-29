9bf68f3f1d2198163eb57a7494533c8c042f4f02
Tyler Davis <tylerrusselldavis@gmail.com>
2021-09-29 17:19:57 -0500

    minor changes

--- wiki/models.py
+++ wiki/models.py
@@ -4,3 +4,7 @@ class Article(models.Model):
     title = models.CharField(max_length=20)
     body = models.TextField()
     pub_date = models.DateTimeField('date published')
+
+
+class Category(models.Model):
+    title = title = models.CharField(max_length=20)
