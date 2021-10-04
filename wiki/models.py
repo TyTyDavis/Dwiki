4c8db36631b9f1dfb1846d18d3fa88b1410d3d37
Tyler Davis <tylerrusselldavis@gmail.com>
2021-10-04 15:31:15 -0500

    Made article data model, set up admin interface

--- wiki/models.py
+++ wiki/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+
+TYPES = (
+                 ('L', 'Locations'),
+                 ('P', 'People'),
+                 ('E', 'Events'),
+                 ('F', 'Factions'),
+                )
+
 class Article(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
     body = models.TextField()
-    pub_date = models.DateTimeField('date published')
+    create_date = models.DateField()
+    type = models.CharField(max_length=1, choices=TYPES)
 
-
-class Category(models.Model):
-    title = title = models.CharField(max_length=20)
+    def __str__(self):
+        return "%s (%s)" %(self.title, self.type)
