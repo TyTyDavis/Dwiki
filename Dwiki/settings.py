7efdbdb556a28a345d09e3450702fc4857976e77
Tyler Davis <tylerrusselldavis@gmail.com>
2021-10-05 15:24:05 -0500

    Switched from SQLite3 to MySQL

--- Dwiki/settings.py
+++ Dwiki/settings.py
@@ -79,11 +79,17 @@ WSGI_APPLICATION = 'Dwiki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dwiki',
+        'USER': 'root',
+        'PASSWORD': config('DBPASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
