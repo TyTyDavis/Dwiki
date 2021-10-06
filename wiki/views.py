38d7f0cbd27e71e8411ec55a759e7f8ed316afb1
Tyler Davis <tylerrusselldavis@gmail.com>
2021-10-06 16:23:45 -0500

    Got very simple template to display article text

--- wiki/views.py
+++ wiki/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from wiki.models import Article
 
 
 def index(request):
-    return render(request,"hello.html",{"name":"Tyler"})
+    a = Article.objects.get(pk=1)
+    return render(request,"hello.html",{"title" : a.title, "body" : a.body})
