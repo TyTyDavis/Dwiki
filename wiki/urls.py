7c6b90010c09d5a0f3c0b8b1a0cb67b95dfd4942
Tyler Davis <tylerrusselldavis@gmail.com>
2021-10-07 14:13:34 -0500

    Set up URL path for articles

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.index, name='index'),
+    path('<str:url_title>/', views.articleView)
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from wiki.models import Article
+from django.shortcuts import get_list_or_404, get_object_or_404
 
 
 def index(request):
     a = Article.objects.get(pk=1)
     return render(request,"hello.html",{"title" : a.title, "body" : a.body})
+
+def articleView(request, url_title):
+    a = get_object_or_404(Article, title=url_title)
+    return render(request,"article.html",{"title" : a.title, "body" : a.body})
