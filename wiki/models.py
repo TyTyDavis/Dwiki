19812249911fe5e4e29b6fcb1b0deb1ef3d0b7da
Tyler Davis <tylerrusselldavis@gmail.com>
2021-10-11 14:25:44 -0500

    defined get_absolute_url for Article, redirect from form working

--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 TYPES = (
                  ('L', 'Locations'),
@@ -14,6 +14,10 @@ class Article(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     type = models.CharField(max_length=1, choices=TYPES)
+    
 
     def __str__(self):
         return "%s (%s)" %(self.title, self.type)
+
+    def get_absolute_url(self):
+        return reverse('article', kwargs={'url_title': self.title})
