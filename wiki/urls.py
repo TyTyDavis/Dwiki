c77c19ff6e7311e749a8ada489af4e1eb06a20b7
Tyler Davis <tylerrusselldavis@gmail.com>
2021-10-11 14:51:17 -0500

    Added UpdateView to edit articles

--- wiki/urls.py
+++ wiki/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('home/', views.index, name='home'),
     path('create/', views.articleCreate.as_view(), name='create'),
+    path('edit/<int:pk>', views.articleUpdate.as_view(), name='edit'),
     path('<str:url_title>/', views.articleView, name='article'),
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from wiki.models import Article
 from django.shortcuts import get_list_or_404, get_object_or_404
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 def index(request):
     a = Article.objects.get(pk=1)
@@ -11,8 +11,13 @@ def index(request):
 def articleView(request, url_title):
 
     a = get_object_or_404(Article, title=url_title)
-    return render(request,"article.html",{"title" : a.title, "body" : a.body})
+    return render(request,"article.html",{"title" : a.title, "body" : a.body, "pk" : a.pk })
 
 class articleCreate(CreateView):
     model = Article
     fields = ['title', 'body', 'type']
+
+class articleUpdate(UpdateView):
+    model = Article
+    fields = ['title', 'body', 'type']
+    template_name_suffix = '_update_form'
